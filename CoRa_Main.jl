fa41370ff86c29940451fab693e1059e9004e1d4
Mariana Gómez-Schiavon, PhD <mgschiavon@users.noreply.github.com>
2023-03-16 09:54:08 -0600

    ExDyn option working

--- CoRa_Main.jl
+++ CoRa_Main.jl
@@ -47,7 +47,7 @@ if(iARG.an=="ExSSs")
             # Calculate the resulting CoRa metric:
             CoRa = fn.CoRa(mm.outFB(ssR), mm.outFB(ssD), mm.outNF(soR), mm.outNF(soD))
             # Print output:
-            writedlm(io, [vcat(p[pert.c],ssR,ssD,soR,soD,DYs)],'\t');
+            writedlm(io, [vcat(p[pert.c],ssR,ssD,soR,soD,CoRa)],'\t');
 			# Reset condition to pre-perturbation state:
             p[pert.c] /= r[i];
         end
@@ -57,31 +57,8 @@ elseif(iARG.an=="ExDyn")
 	p = copy(pO);
 	open(string("OUT_ExDyn_",iARG.mm,"_",iARG.ex,"_",iARG.pp,"_",iARG.ax,".txt"), "w") do io
 		writedlm(io, [vcat("FB","rho","time",[string(i) for i in mm.odeNF.syms])],'\t');
-		rtol = 1e-12;
-		uns = 0;
-		ssR = ones(length(mm.odeFB.syms));
-		soR = ones(length(mm.odeNF.syms));
-		while(rtol >= 1e-24)
-			# Reference steady state:
-			ssR = fn.SS(mm.odeFB, p, ssR, rtol, uns);
-			# Locally analogous system reference steady state:
-			mm.localNF(p,ssR);
-			soR = fn.SS(mm.odeNF, p, soR, rtol, uns);
-			if(abs(mm.outFB(ssR) - mm.outNF(soR)) > 1e-4)
-				rtol *= 1e-3;
-				if(rtol < 1e-24)
-					println("ERROR: Check NF system (reltol=",rtol*1e3,").")
-					println(vcat(pert.p,i,[p[i] for i in mm.odeFB.params],mm.outFB(ssR),mm.outNF(soR)))
-					#throw(DomainError("x-("))
-					if(abs(mm.outFB(ssR) - mm.outNF(soR))/mm.outFB(ssR) > 0.01)
-						flg1 = 0;
-						println("SS results excluded!")
-					end
-				end
-			else
-				break
-			end
-		end
+		# Reference steady state:
+		ssR, soR, rtol = fn.SSandCheck(p, x0, 1e-12, mm)
 		# Feedback system:
 		x = fn.Dyn(mm.odeFB, p, ssR, 500.0);
 		for i in 1:length(x.t)
@@ -92,7 +69,7 @@ elseif(iARG.an=="ExDyn")
 		for i in 1:length(x.t)
 			writedlm(io, [vcat(1,p[iARG.pp],x.t[i]+500.0,x.u[i],"NaN")],'\t');
 		end
-		ssD = fn.SS(mm.odeFB, p, ssR, rtol, uns);
+		ssD = fn.SS(mm.odeFB, p, ssR, rtol);
 		writedlm(io, [vcat(1,p[iARG.pp],"Inf",ssD,"NaN")],'\t');
 		p[pert.p] /= pert.d;
 		# No-Feedback system:
@@ -105,7 +82,7 @@ elseif(iARG.an=="ExDyn")
 		for i in 1:length(x.t)
 			writedlm(io, [vcat(0,p[iARG.pp],x.t[i]+500.0,x.u[i])],'\t');
 		end
-		soD = fn.SS(mm.odeNF, p, soR, rtol, uns);
+		soD = fn.SS(mm.odeNF, p, soR, rtol);
 		writedlm(io, [vcat(0,p[iARG.pp],"Inf",soD)],'\t');
 		p[pert.p] /= pert.d;
 	end
