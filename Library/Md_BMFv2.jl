7739416e35cdc4311cd36ba90882d3f2f11f1d76
Mariana Gómez-Schiavon, PhD <mgschiavon@users.noreply.github.com>
2023-03-31 15:01:11 -0600

    BMFv2 CoRams
    Update & run

--- Library/Md_BMFv2.jl
+++ Library/Md_BMFv2.jl
@@ -1,7 +1,7 @@
 # Brink motif feedback (v02)
 #   with Y represses A synthesis,
 
-# Julia v.1.1.1
+# Julia v.1.8
 
 module mm
 	# Required libraries
@@ -15,18 +15,17 @@ module mm
 		dB  =  mB                   - (g * B) - (eP * A * B) + (e0 * C) - (bI * B * U)
 		dC  =                       - (g * C) + (eP * A * B) - (e0 * C) + (bA * A * Us)
 		dU  =  mU                   - (g * U) + (bA * A * Us) - (bI * B * U)
-		dUs =                      - (g * Us) - (bA * A * Us) + (bI * B * U)
-	end g mY gY mA kD mB mU eP e0 bA bI mYs gYs;
+		dUs =                       - (g * Us) - (bA * A * Us) + (bI * B * U)
+	end g mY gY mA kD mB mU eP e0 bA bI mAs;
 	# ODE system without feedback
 	odeNF = @ode_def begin
 		dY  = (mY * U)              - ((g + gY) * Y)
-		dA  = (mA * (kD/(kD + Ys))) - (g * A) - (eP * A * B) + (e0 * C) - (bA * A * Us)
+		dA  =  mAs                  - (g * A) - (eP * A * B) + (e0 * C) - (bA * A * Us)
 		dB  =  mB                   - (g * B) - (eP * A * B) + (e0 * C) - (bI * B * U)
 		dC  =                       - (g * C) + (eP * A * B) - (e0 * C) + (bA * A * Us)
 		dU  =  mU                   - (g * U) + (bA * A * Us) - (bI * B * U)
-		dUs =                      - (g * Us) - (bA * A * Us) + (bI * B * U)
-		dYs = mYs - ((g + gYs) * Ys)
-	end g mY gY mA kD mB mU eP e0 bA bI mYs gYs;
+		dUs =                       - (g * Us) - (bA * A * Us) + (bI * B * U)
+	end g mY gY mA kD mB mU eP e0 bA bI mAs;
 
 	# Define system's output (total Y):
 	function outFB(ss)
@@ -38,7 +37,6 @@ module mm
 
 	# Define locally analogous system:
 	function localNF(p,ss)
-		p[:mYs] = p[:mY] * ss[5];
-		p[:gYs] = p[:gY];
+		p[:mAs] = p[:mA] * (p[:kD]/(p[:kD] + ss[1]));
 	end;
 end
