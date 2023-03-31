8aa2af00a4ae022b2175dd586ec44e42b1c74754
Mariana Gómez-Schiavon, PhD <mgschiavon@users.noreply.github.com>
2023-03-31 14:48:39 -0600

    ATFv2 CoRams
    Update & run

--- Library/Md_ATFv2.jl
+++ Library/Md_ATFv2.jl
@@ -1,7 +1,7 @@
 # Antithetic feedback (v02)
 #   with active W in complex form
 
-# Julia v.1.1.1
+# Julia v.1.8
 
 module mm
 	# Required libraries
@@ -14,15 +14,14 @@ module mm
 		dU = (mU * Y)       - ((g + gU) * U) - (eP * U * W) + ((e0 + gW) * C)
 		dW =    mW          - ((g + gW) * W) - (eP * U * W) + ((e0 + gU) * C)
 		dC =                - ((g + eM) * C) + (eP * U * W) - ((gU + gW + e0) * C)
-	end g mY gY mU gU mW gW e0 eP eM mYs gYs;
+	end g mY gY mU gU mW gW e0 eP eM mUs;
 	# ODE system without feedback
 	odeNF = @ode_def begin
 		dY  = (mY * (W + C)) - ((g + gY) * Y)
-		dU  = (mU * Ys)      - ((g + gU) * U) - (eP * U * W) + ((e0 + gW) * C)
+		dU  = (mUs)          - ((g + gU) * U) - (eP * U * W) + ((e0 + gW) * C)
 		dW  =    mW          - ((g + gW) * W) - (eP * U * W) + ((e0 + gU) * C)
 		dC  =                - ((g + eM) * C) + (eP * U * W) - ((gU + gW + e0) * C)
-		dYs =    mYs         - ((g + gYs) * Ys)
-	end g mY gY mU gU mW gW e0 eP eM mYs gYs;
+	end g mY gY mU gU mW gW e0 eP eM mUs;
 
 	# Define system's output (total Y):
 	function outFB(ss)
@@ -34,10 +33,6 @@ module mm
 
 	# Define locally analogous system:
 	function localNF(p,ss)
-		p[:mYs] = p[:mY] * (ss[3] + ss[4]);
-		p[:gYs] = p[:gY];
+		p[:mUs] = p[:mU] * ss[1];
 	end;
-
-	# dU + dC = (mU * Y) - ((g + gU) * (U + C)) - (eM * C)
-	# dW + dC =    mW    - ((g + gW) * (W + C)) - (eM * C)
 end
