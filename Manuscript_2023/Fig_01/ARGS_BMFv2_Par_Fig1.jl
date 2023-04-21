42d10dc363edb9691378b1562c116e3f0b2e8b02
Mariana Gómez-Schiavon, PhD <mgschiavon@users.noreply.github.com>
2023-04-21 13:31:13 -0600

    Updating "central" value -BMFv2
    mY=1, x1, Y approx. 10

--- Manuscript_2023/Fig_01/ARGS_BMFv2_Par_Fig1.jl
+++ Manuscript_2023/Fig_01/ARGS_BMFv2_Par_Fig1.jl
@@ -1,7 +1,7 @@
 # Kinetic parameters
 p = Dict([
     :g   => 0.01,      # Dilution rate (e.g. [0.01,0.24] 1/min)
-    :mY  => 0.125,     # Y synthesis rate dependent of W (nM/min)
+    :mY  => 1.0,       # Y synthesis rate dependent of W (nM/min)
     :gY  => 0.1,       # Y degradation rate (e.g. [0.0301,0.0408] 1/min, with degODC)
     :mA  => 0.372,     # A maximum synthesis rate repressed by Y (nM/min)
     :kD  => 1,         # Y repression KD (nM)
