ee3b563a853143971fe40e82c1bb36ecc2b65f88
Mariana Gómez-Schiavon, PhD <mgschiavon@users.noreply.github.com>
2023-05-19 11:20:03 -0600

    Updating "central" value -FFLv1
    mY=1, x1, Y approx. 10

--- Manuscript_2023/Fig_01/ARGS_FFLv1_Par_Fig1.jl
+++ Manuscript_2023/Fig_01/ARGS_FFLv1_Par_Fig1.jl
@@ -1,12 +1,12 @@
 # Kinetic parameters
 p = Dict([
     :g   => 0.01,      # Dilution rate (e.g. [0.01,0.24] 1/min)
-    :mY  => 0.125,     # Y synthesis rate dependent of W (nM/min)
+    :mY  => 1.0,       # Y synthesis rate dependent of W (nM/min)
     :gY  => 0.1,       # Y degradation rate (e.g. [0.0301,0.0408] 1/min, with degODC)
-    :mU  => 0.0334,    # U maximum synthesis rate repressed by Y (nM/min)
+    :mU  => 0.1,       # U maximum synthesis rate repressed by Y (nM/min)
     :kD  => 1,         # Y repression KD (nM)
     :gU  => 0.01,      # U degradation rate (e.g. [0.0301,0.0408] 1/min, with degODC)
-    :mW  => 0.125,     # W synthesis rate dependent of U (1/min)
+    :mW  => 0.028,     # W synthesis rate dependent of U (1/min)
     :gW  => 0.01,      # W degradation rate (e.g. [0.0301,0.0408] 1/min, with degODC)
     :mUs => NaN,       # LOCAL: U constitutive synthesis rate in the locally analogous system (mU*Yss)
 ]);
--- Run_CoRa.jl
+++ Run_CoRa.jl
@@ -1,7 +1,7 @@
 ## Running in julia terminal
 	#cd("C:\\MyLibrary\\Location\\")
 	using Pkg; Pkg.activate(".");
-	iARG = (mm = "FDPv2",		# Label for motif file
+	iARG = (mm = "FFLv1",		# Label for motif file
 			ex = "Fig1",		# Label for parameters file
 			pp = :mY,			# Label for perturbation type
 			ax = :mY,			# Label for condition/environment
