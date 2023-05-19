c317b7bafcffcf84f6d38d3b13ee69c0dba62713
Mariana Gómez-Schiavon, PhD <mgschiavon@users.noreply.github.com>
2023-05-19 10:24:25 -0600

    Updating "central" value -FADv1
    mY=1, x1, Y approx. 10

--- Manuscript_2023/Fig_01/ARGS_FADv1_Par_Fig1.jl
+++ Manuscript_2023/Fig_01/ARGS_FADv1_Par_Fig1.jl
@@ -1,9 +1,9 @@
 # Kinetic parameters
 p = Dict([
     :g   => 0.01,      # Dilution rate (e.g. [0.01,0.24] 1/min)
-    :mY  => 0.125,     # Y synthesis rate dependent of W (nM/min)
+    :mY  => 1.0,       # Y synthesis rate dependent of W (nM/min)
     :gY  => 0.1,       # Y degradation rate (e.g. [0.0301,0.0408] 1/min, with degODC)
-    :mU  => 0.125,     # U synthesis rate dependent of Y (nM/min)
+    :mU  => 0.0152,    # U synthesis rate dependent of Y (nM/min)
     :gU  => 0.05,      # U degradation rate (e.g. [0.0301,0.0408] 1/min, with degODC)
     :mW  => 0.10,      # W constitutive synthesis rate (e.g. [0.005,0.02] nM/min)
     :gW  => 0.0001,    # W degradation rate (e.g. [0.0301,0.0408] 1/min, with degODC)
