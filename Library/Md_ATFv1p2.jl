f3628f3c7afe417338139c5f13f77688f1715c74
Mariana Gómez-Schiavon, PhD <mgschiavon@users.noreply.github.com>
2023-05-31 11:54:24 -0600

    Updated locally analogous system
    To use a constant parameter instead of auxiliary species.

--- Library/Md_ATFv1p2.jl
+++ Library/Md_ATFv1p2.jl
@@ -2,7 +2,7 @@
 #   with inactive W in complex form
 #   considering a more complex process
 
-# Julia v.1.1.1
+# Julia v.1.8
 
 module mm
 	# Required libraries
@@ -17,17 +17,16 @@ module mm
 		dC  =                       - ((g + eM) * C) + (eP * U * W) - ((gU + gW + e0) * C)
 		dY0 = (m0 * W)              - ((g + gY) * Y0)
 		dY1 = (m1 * (k1/(Y0 + k1))) - ((g + gY) * Y1)
-	end g mY kD gY mU gU mW gW e0 eP eM m0 m1 k1 mYs gYs;
+	end g mY kD gY mU gU mW gW e0 eP eM m0 m1 k1 mUs;
 	# ODE system without feedback
 	odeNF = @ode_def begin
 		dY  = (mY * (kD/(Y1 + kD))) - ((g + gY) * Y)
-		dU  = (mU * Ys) - ((g + gU) * U) - (eP * U * W) + ((e0 + gW) * C)
+		dU  =   (mUs)   - ((g + gU) * U) - (eP * U * W) + ((e0 + gW) * C)
 		dW  =    mW     - ((g + gW) * W) - (eP * U * W) + ((e0 + gU) * C)
 		dC  =           - ((g + eM) * C) + (eP * U * W) - ((gU + gW + e0) * C)
 		dY0 = (m0 * W)              - ((g + gY) * Y0)
 		dY1 = (m1 * (k1/(Y0 + k1))) - ((g + gY) * Y1)
-		dYs =    mYs    - ((g + gYs) * Ys)
-	end g mY kD gY mU gU mW gW e0 eP eM m0 m1 k1 mYs gYs;
+	end g mY kD gY mU gU mW gW e0 eP eM m0 m1 k1 mUs;
 
 	# Define system's output (total Y):
 	function outFB(ss)
@@ -39,7 +38,6 @@ module mm
 
 	# Define locally analogous system:
 	function localNF(p,ss)
-		p[:mYs] = p[:mY] * (p[:kD]/(ss[6] + p[:kD]));
-		p[:gYs] = p[:gY];
+		p[:mUs] = p[:mU] * ss[1];
 	end;
 end
--- Library/Md_ATFv1p3.jl
+++ Library/Md_ATFv1p3.jl
@@ -2,7 +2,7 @@
 #   with inactive W in complex form
 #   considering a more complex process
 
-# Julia v.1.1.1
+# Julia v.1.8
 
 module mm
 	# Required libraries
@@ -17,17 +17,16 @@ module mm
 		dC  =                                   - ((g + eM) * C) + (eP * U * W) - ((gU + gW + e0) * C)
 		dY0 = (m0 * W)                          - ((g + gY) * Y0)
 		dY1 = (m1 * Y0) + (mP * (Y1/(Y1 + kP))) - ((g + gY) * Y1)
-	end g mY gY mU gU mW gW e0 eP eM m0 m1 mP kP mYs gYs;
+	end g mY gY mU gU mW gW e0 eP eM m0 m1 mP kP mUs;
 	# ODE system without feedback
 	odeNF = @ode_def begin
 		dY  = (mY * Y1)                         - ((g + gY) * Y)
-		dU  = (mU * Ys)                         - ((g + gU) * U) - (eP * U * W) + ((e0 + gW) * C)
+		dU  = (mUs)                             - ((g + gU) * U) - (eP * U * W) + ((e0 + gW) * C)
 		dW  =    mW                             - ((g + gW) * W) - (eP * U * W) + ((e0 + gU) * C)
 		dC  =                                   - ((g + eM) * C) + (eP * U * W) - ((gU + gW + e0) * C)
 		dY0 = (m0 * W)                          - ((g + gY) * Y0)
 		dY1 = (m1 * Y0) + (mP * (Y1/(Y1 + kP))) - ((g + gY) * Y1)
-		dYs =    mYs    - ((g + gYs) * Ys)
-	end g mY gY mU gU mW gW e0 eP eM m0 m1 mP kP mYs gYs;
+	end g mY gY mU gU mW gW e0 eP eM m0 m1 mP kP mUs;
 
 	# Define system's output (total Y):
 	function outFB(ss)
@@ -39,7 +38,6 @@ module mm
 
 	# Define locally analogous system:
 	function localNF(p,ss)
-		p[:mYs] = p[:mY] * ss[6];
-		p[:gYs] = p[:gY];
+		p[:mUs] = p[:mU] * ss[1];
 	end;
 end
