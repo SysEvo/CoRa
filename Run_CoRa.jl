680a978249aa0f7a118f8ddb242c19e1cfbeebd3
Mariana Gómez-Schiavon, PhD <mgschiavon@users.noreply.github.com>
2023-10-13 11:00:57 -0600

    Run Fig. 2 - ATFv1p3
    Old parameters.

--- Run_CoRa.jl
+++ Run_CoRa.jl
@@ -1,9 +1,9 @@
 ## Running in julia terminal
 	#cd("C:\\MyLibrary\\Location\\")
 	using Pkg; Pkg.activate(".");
-	iARG = (mm = "ATFv1",		# Label for motif file
+	iARG = (mm = "ATFv1p3",		# Label for motif file
 			ex = "Fig2",		# Label for parameters file
 			pp = :mY,			# Label for perturbation type
 			ax = :mY,			# Label for condition/environment
-			an = "ExSSs");		# Chose analysis type (Options: ExSSs, ExDyn, CoRams, OptCoRa)
+			an = "CoRams");		# Chose analysis type (Options: ExSSs, ExDyn, CoRams, OptCoRa)
 	include("CoRa_Main.jl")
