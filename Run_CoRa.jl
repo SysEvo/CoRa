12feb5800613922267316dc8e53771351a6927af
Mariana Gómez-Schiavon, PhD <mgschiavon@users.noreply.github.com>
2023-10-13 10:54:05 -0600

    Run Fig. 2 - ATFv1p2
    Old parameters.

--- Run_CoRa.jl
+++ Run_CoRa.jl
@@ -1,7 +1,7 @@
 ## Running in julia terminal
 	#cd("C:\\MyLibrary\\Location\\")
 	using Pkg; Pkg.activate(".");
-	iARG = (mm = "ATFv1",		# Label for motif file
+	iARG = (mm = "ATFv1p2",		# Label for motif file
 			ex = "Fig2",		# Label for parameters file
 			pp = :mY,			# Label for perturbation type
 			ax = :mY,			# Label for condition/environment
