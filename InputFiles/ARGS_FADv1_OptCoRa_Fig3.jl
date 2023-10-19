a3758804a83544063657a1815f68a5f63980895e
Mariana Gómez-Schiavon, PhD <mgschiavon@users.noreply.github.com>
2023-10-19 15:21:59 -0600

    First long test run
    The test failed because Check() --if the rtol limit is exceeded-- tries to print perturbation information ('pert.p'; line 91, FN_CoRa.jl), when this variable hasn't been shared.

--- InputFiles/ARGS_FADv1_OptCoRa_Fig3.jl
+++ InputFiles/ARGS_FADv1_OptCoRa_Fig3.jl
@@ -2,7 +2,7 @@ mrw  = (pOp  = [:mU,:mW,:eP],	# Parameters to optimize
 		pMin = [-3,-3,-3],		# Minimum parameter value to explore (log10)
 		pMax = [3,3,3],			# Maximum parameter value to explore (log10)
 		runs = 3,				# Number of optimization runs
-		iter = 10,				# Number of iterations per optimization run
+		iter = 1000,				# Number of iterations per optimization run
 		cov  = [0.1,0.1,0.1],	# Covariance to calculate parameter random walk
 		M    = 10,				# "Mutation step size" for multiplicative random walk
 		ranP = 0,				# Flag for random initial parameters
