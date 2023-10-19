6728363f753a99ce1333ac6fa770c63d5fef218f
Mariana Gómez-Schiavon, PhD <mgschiavon@users.noreply.github.com>
2023-10-19 13:10:19 -0600

    Input files for testing OptCoRa

--- Run_CoRa.jl
+++ Run_CoRa.jl
@@ -1,9 +1,9 @@
 ## Running in julia terminal
 	#cd("C:\\MyLibrary\\Location\\")
 	using Pkg; Pkg.activate(".");
-	iARG = (mm = "ATFv2p3",		# Label for motif file
-			ex = "Fig2",		# Label for parameters file
+	iARG = (mm = "FADv1",		# Label for motif file
+			ex = "Fig3",		# Label for parameters file
 			pp = :mY,			# Label for perturbation type
 			ax = :mY,			# Label for condition/environment
-			an = "ExSSs");		# Chose analysis type (Options: ExSSs, ExDyn, CoRams, OptCoRa)
+			an = "OptCoRa");		# Chose analysis type (Options: ExSSs, ExDyn, CoRams, OptCoRa)
 	include("CoRa_Main.jl")
